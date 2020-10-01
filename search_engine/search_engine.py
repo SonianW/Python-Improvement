59ddde7aa57366dd0df6c76cfbbb4b65771061e0
SonianW <705842688@qq.com>
2020-10-01 21:15:20 +0800

    load test corpus

--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -3,21 +3,22 @@ class SearchEngineBase(object):
         pass
 
     def add_corpus(self, file_path):
-        with open(file_path, 'r') as f:
+        with open(file_path, "r") as f:
             text = f.read()
         self.process_corpus(file_path, text)
 
     def process_corpus(self, id, text):
-        raise Exception('process_corpus not implemented.')
+        raise Exception("process_corpus not implemented.")
 
     def search(self, query):
-        raise Exception('search not implemented')
+        raise Exception("search not implemented")
+
 
 class SimpleEngine(SearchEngineBase):
     def __init__(self):
         super(SimpleEngine, self).__init__()
         self.__id_to_texts = {}
-    
+
     def process_corpus(self, id, text):
         self.__id_to_texts[id] = text
 
@@ -28,13 +29,24 @@ class SimpleEngine(SearchEngineBase):
                 results.append(id)
         return results
 
+
 def main(search_engine):
-    for file_path in ['1.txt', '2.txt', '3.txt', '4.txt', '5.txt']:
+    for file_path in [
+        "./corpus/1.txt",
+        "./corpus/2.txt",
+        "./corpus/3.txt",
+        "./corpus/4.txt",
+        "./corpus/5.txt",
+    ]:
         search_engine.add_corpus(file_path)
-    
+
     while True:
         query = input()
         results = search_engine.search(query)
-        print('found {} result(s):'.format(len(results)))
+        print("found {} result(s):".format(len(results)))
         for result in results:
             print(result)
+
+
+search_engine = SimpleEngine()
+main(search_engine)
