9cbd989800b94ebf364a58adba34636d2318892f
SonianW <705842688@qq.com>
2020-10-01 23:52:13 +0800

    create BOWInvertedIndexEngine

--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -46,7 +46,7 @@ class BOWEngine(SearchEngineBase):
         query_words = self.parse_text_to_words(query)
         results = []
         for id, words in self.__id_to_words.items():
-            if self.qeury_match(query_words, words):
+            if self.query_match(query_words, words):
                 results.append(id)
         return results
 
@@ -65,7 +65,55 @@ class BOWEngine(SearchEngineBase):
         word_list = filter(None, word_list)
         return set(word_list)
 
+class BOWInvertedIndexEngine(SearchEngineBase):
+    def __init__(self):
+        super(BOWInvertedIndexEngine, self).__init__()
+        self.inverted_index = {}
+    
+    def process_corpus(self, id, text):
+        words = self.parse_text_to_words(text)
+        for word in words:
+            if word not in self.inverted_index:
+                self.inverted_index[word] = []
+            self.inverted_index[word].append(id)
 
+    def search(self, query):
+        query_words = list(self.parse_text_to_words(query))
+        query_words_index = list()
+        for query_word in query_words:
+            if query_word not in self.inverted_index:
+                return []
+        
+        result = []
+        while True:
+            current_ids = []
+
+            for idx, query_word in enumerate(query_words):
+                current_index = query_words_index[idx]
+                current_inverted_list = self.inverted_index[query_word]
+            
+                if current_index >= len(current_inverted_list):
+                    return result
+
+                current_ids.append(current_inverted_list[current_index])
+
+            if all(x == current_ids[0] for x in current_ids):
+                result.append(current_ids[0])
+                query_words_index = [x + 1 for x in query_words_index]
+                continue
+
+            min_val = min(current_ids)
+            min_val_pos = current_ids.insert(min_val)
+            query_words_index[min_val_pos] += 1
+
+    @staticmethod
+    def parse_text_to_words(text):
+        text = re.sub(r"[^\w]", " ", text)
+        text = text.lower()
+        word_list = text.split(" ")
+        word_list = filter(None, word_list)
+        return set(word_list)
+        
 # %%%%% test code
 def main(search_engine):
     for file_path in [
@@ -85,5 +133,5 @@ def main(search_engine):
             print(result)
 
 
-search_engine = SimpleEngine()
+search_engine = BOWEngine()
 main(search_engine)
